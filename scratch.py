57cdd5622be8a6ab7bafba004ef6d4edcefc6c97
kartikwff-bit <kartik.wff@gmail.com>
2026-05-11 16:26:40 +0530

    Delete scratch.py

--- scratch.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import json
-
-df = pd.read_excel(r"c:\Users\Kartik\OneDrive\Desktop\RM's\NAME.xlsx")
-# Replace NaN with None for json compatibility
-df = df.where(pd.notnull(df), None)
-print(json.dumps(df.to_dict('records')))
